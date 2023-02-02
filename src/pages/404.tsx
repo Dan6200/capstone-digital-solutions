58f6e57c7e1762f8a8ed976e9092d81b5fcded5f
Daniel Nyong <danielnyong66@gmail.com>
2023-02-02 21:16:34 +0100

    mobile 404 page complete

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import Footer from '../components/footer'
+import Header from '../components/header'
 
 const NotFound = () => (
-    <div className="flex bg-white flex-col gap-[10vw] items-center justify-center w-full h-screen">
-        <h1 className="text-6xl font-semibold text-center capitalize md:text-4xl sm:text-5xl font-heading text-black-900">
-            <span className="text-transparent bg-clip-text bg-gradient-to-br from-secondary to-primary">
+    <div className="flex flex-col items-center w-full h-auto bg-white">
+        <Header />
+        <h1 className="text-5xl font-semibold mb-[15vw] text-center capitalize md:text-3xl sm:text-2xl font-heading w-[90%] text-black-900">
+            <span className="text-black-900">
                 This page has not been created yet. Sorry...
             </span>
         </h1>
         <button
-            className="p-[1pt] shadow-xl text-white active:opacity-75 font-heading font-semibold uppercase rounded-lg w-[40vw] sm:w-[30vw] md:w-[20vw] lg:w-[15vw] hover:from-primary hover:shadow-2xl hover:to-secondary bg-gradient-to-r from-secondary to-primary inline h-auto text-center"
+            className="p-[1pt] shadow-xl mb-[60vw] leading-[12vw] text-white active:opacity-75 font-heading font-semibold uppercase rounded-lg w-[40vw] sm:w-[30vw] md:w-[20vw] lg:w-[15vw] hover:from-black-700 hover:shadow-2xl hover:to-black-900 bg-gradient-to-r from-black-900 to-black-700 inline h-auto text-center"
             onClick={() => window.history.back()}
         >
             Go Back
         </button>
+        <Footer />
     </div>
 )
 
