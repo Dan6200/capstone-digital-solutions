da255a7144fff92269879023ee832102d3f28c96
Daniel Nyong <danielnyong66@gmail.com>
2023-02-13 11:43:22 +0100

    fixing spacing issues

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -7,21 +7,23 @@ import { content } from '../data/services/content'
 const Services: React.FC = () => (
     <div className="bg-white">
         <Header />
-        {content.map((subcontent) => (
-            <div
-                id={subcontent.heading.replaceAll(' ', '-')}
-                className="w-[90%] mx-auto text-3xl text-left  lg:text-lg xl:text-md md:text-xl sm:text-2xl pt-[35vw] sm:pt-[25vw] md:pt-[15vw] lg:pt-[10vw]"
-            >
-                <h3 className="text-4xl md:text-3xl lg:text-2xl xl:text-xl mb-[4vw] italic w-[90%] font-light capitalize border-b-2 border-primary text-black-900 font-heading">
-                    {subcontent.heading}
-                </h3>
-                {subcontent.body.map((paragraph) => (
-                    <p className="mb-[4vw] lg:mb-[2vw] break-words text-black-800 font-paragraph">
-                        {paragraph}
-                    </p>
-                ))}
-            </div>
-        ))}
+        <div className="jack pt-[30vw] sm:pt-[20vw] md:pt-[15vw] lg:pt-[10vw]">
+            {content.map((subcontent) => (
+                <div
+                    id={subcontent.heading.replaceAll(' ', '-')}
+                    className="w-[90%] mx-auto text-3xl text-left  lg:text-lg xl:text-md md:text-xl sm:text-2xl pt-[15vw] sm:pt-[10vw] md:pt-[5vw]"
+                >
+                    <h3 className="text-4xl md:text-3xl lg:text-2xl xl:text-xl mb-[4vw] italic w-[90%] font-light capitalize border-b-2 border-primary text-black-900 font-heading">
+                        {subcontent.heading}
+                    </h3>
+                    {subcontent.body.map((paragraph) => (
+                        <p className="mb-[4vw] lg:mb-[2vw] break-words text-black-800 font-paragraph">
+                            {paragraph}
+                        </p>
+                    ))}
+                </div>
+            ))}
+        </div>
         <Footer />
     </div>
 )
