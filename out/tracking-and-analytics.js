aa9fe86b50ae5e16e26502d89fd4899c8efff395
Daniel Nyong <danielnyong66@gmail.com>
2023-02-18 10:38:18 +0100

    Removed out file and added it to gitignore

--- out/tracking-and-analytics.js
+++
@@ -1,6 +0,0 @@
-window.smartlook||(function(d) {
-  var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-  var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-  c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-  })(document);
-  smartlook('init', '32e217cba4d136d2735a03cd06194dc0f0dca9a4', { region: 'eu' });
