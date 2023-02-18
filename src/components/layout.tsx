83b8bf712e190b64ec6773a531305c19e5463aea
Daniel Nyong <danielnyong66@gmail.com>
2023-02-18 20:29:45 +0100

    fixed bug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,14 +4,12 @@ import Favicon from './favicon'
 import Footer from './footer'
 import Header from './header'
 import SEO from './seo'
-import Tracking from './tracking-and-analytics'
 
 export default function Layout({ children }) {
     const { description, title } = seoConfig
     return (
         <div className="bg-white">
             <SEO description={description} title={title} />
-            <Tracking />
             <Favicon />
             <Header />
             <main className="scroll-smooth">{children}</main>
